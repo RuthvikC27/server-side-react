a0955277f60a77cf90d3bde4bd22590a00297be1
Ruthvik Vanam <ruthvikvanam27@gmail.com>
2020-09-05 21:24:40 +0530

    initial

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const React = require("react");
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
+
+const app = express();
+ 
+app.get('/', (req, res) => {
+    const content = renderToString(<Home />);
+
+    res.send(content);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('App listening on http://localhost:' + PORT);
+});
