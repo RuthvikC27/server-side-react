2d485301a0d326297f9ca96bad9d74b2593b2eae
Ruthvik Vanam <ruthvikvanam27@gmail.com>
2020-09-06 13:49:01 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import React from 'react';
-import {renderToString} from 'react-dom/server';
+import { renderToString } from 'react-dom/server';
 
 import Home from './client/components/Home';
 
 const app = express();
 
 
-app.use(express.static('public')); 
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     const content = renderToString(<Home />);
