68655d1ab0ac5852cdd545d3f7a7b82fb9a6a08a
Ruthvik Vanam <ruthvikvanam27@gmail.com>
2020-09-06 18:44:21 +0530

    initial

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,26 @@
+import 'babel-polyfill'
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from './components/Home';
 
-ReactDOM.hydrate(<Home />, document.getElementById('root'));
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './Routes.js'
+
+import reducers from './reducers'
+
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
+
+
+ReactDOM.hydrate(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
+import 'babel-polyfill';
+
 import express from 'express';
 import renderer from './helpers/renderer';
+import createStore from './helpers/createStore'
+import reducers from './client/reducers'
+
 
 const app = express();
 
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    
+app.get('*', (req, res) => {
+    const store = createStore();
+    // Some logic to initialize
+    // and load data into the store
 
-    res.send(renderer());
+    res.send(renderer(req, store));
 });
 
 const PORT = 3000;
